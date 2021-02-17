77087e5ef4edc4dc613c38cadb34282e9862cb9d
Fabian <guilhermeferreirafabian@gmail.com>
2021-02-17 16:04:16 -0300

    Exchange db access to URI

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,9 @@
 const { Pool } = require('pg');
 
+
+
 const pool = new Pool({
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
